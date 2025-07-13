2dd5f71ac98aadb38d4783dec8f279c70cf8891d
EhabAhmed <e.ahmed2684@gmail.com>
2025-07-13 14:14:27 +0300

    edit phone namber mapping in trainee data

--- Core/Services/MappingProfiles/TraineeMaper.cs
+++ Core/Services/MappingProfiles/TraineeMaper.cs
@@ -28,6 +28,7 @@ namespace Services.MappingProfiles
             CreateMap<Trainee, TraineeDataToReturnDto>()
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.AppUser.FirstName))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.AppUser.LastName))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.AppUser.PhoneNumber))
                 .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
                 .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
